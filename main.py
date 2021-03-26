6fd96ab6ac5ec5ce317c3cdaf064b9b639962820
ClemLcs <locascio.clement@gmail.com>
2021-03-26 08:21:00 +0100

    Modification du main

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from Ratio import Ratio
-def main():
-    print("Saisissez un premier nombre rationnel:")
-    numerateur = input("Numérateur : ")
-    denominateur = input("Dénominateur : ")
+def fraction (numerateur, denominateur):
     try:
-        Ratio(numerateur,denominateur)
+        return Ratio(numerateur, denominateur)
     except ValueError:
-        print("Veuillez entrer un nombre positif")
+        print("Vous devez saisir des nombres entiers.")
     except Exception as e:
         print(type(e))
         print(e)
+
+def main():
+    #Demande ma première fraction
+    print("Saisissez un premier nombre rationnel :")
+    numerateur1 = input("Numérateur : ")
+    denominateur1 = input("Dénominateur : ")
+
+    r1 = fraction(numerateur1, denominateur1)
+
+    #Demande ma deuxième fractions
+    print("Saisissez un deuxième nombre rationnel :")
+    numerateur2 = input("Numérateur : ")
+    denominateur2 = input("Dénominateur : ")
+
+    try:
+        r2 = Ratio(numerateur2,denominateur2)
+    except ValueError:
+        print("Vous devez saisir des nombres entiers.")
+        main()
 
 main()
