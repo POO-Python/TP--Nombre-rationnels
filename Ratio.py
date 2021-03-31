983bea361fea5e7e2b3f7df2bc640db7e2c3ba19
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-31 09:12:09 +0200

    Ajout de la gestion complète de la saisie des deux fractions entré par l'utilisateur

--- Ratio.py
+++ Ratio.py
@@ -5,10 +5,17 @@ class Ratio(object):
 
         if isinstance(numerateur, int) == False  or isinstance(denominateur, int) == False:
             raise ValueError
+        elif denominateur == 0:
+            raise ZeroDivisionError
         else:
             self.numerateur = numerateur
             self.denominateur = denominateur
 
+    #Méthodes
+
+    def __str__ (self):
+
+        return str(self.numerateur) +  " / " + str(self.denominateur)
 
     #Getters
     @property
--- main.py
+++ main.py
@@ -1,30 +1,67 @@
 from Ratio import Ratio
-def fraction (numerateur, denominateur):
+
+
+#Initialisation des variables
+r1 = None
+r2 = None
+
+numerateur1 = None
+denominateur1 = None
+
+numerateur2 = None
+denominateur2 = None
+
+#Fonction qui permet de saisir la première fraction
+#Renvoie une instance de la Classe Ratio
+
+def saisie_1er_fraction():
+    #Demande ma première fraction
+    print("\nSaisissez un premier nombre rationnel :")
+    numerateur1 = input("Numérateur : ")
+    denominateur1 = input("Dénominateur : ")
     try:
-        return Ratio(numerateur, denominateur)
+         return Ratio(int(numerateur1), int(denominateur1))
+
     except ValueError:
-        print("Vous devez saisir des nombres entiers.")
+        print("Vous devez saisir des nombres entiers.\n")
+        saisie_1er_fraction()
+    except ZeroDivisionError:
+        print("Un dénominateur ne peut pas être égal à zéro.\n")
+        saisie_1er_fraction()
     except Exception as e:
         print(type(e))
         print(e)
 
-def main():
-    #Demande ma première fraction
-    print("Saisissez un premier nombre rationnel :")
-    numerateur1 = input("Numérateur : ")
-    denominateur1 = input("Dénominateur : ")
+#Fonction qui permet de saisir la deuxième fractions
+#Renvoie une instance de la Classe Ratio
 
-    r1 = fraction(numerateur1, denominateur1)
-
+def saisie_2eme_fractions():
     #Demande ma deuxième fractions
-    print("Saisissez un deuxième nombre rationnel :")
+    print("\nSaisissez un deuxième nombre rationnel :")
     numerateur2 = input("Numérateur : ")
     denominateur2 = input("Dénominateur : ")
 
     try:
-        r2 = Ratio(numerateur2,denominateur2)
+        return Ratio(int(numerateur2),int(denominateur2))
     except ValueError:
-        print("Vous devez saisir des nombres entiers.")
-        main()
+        print("Vous devez saisir des nombres entiers.\n")
+        saisie_2eme_fractions()
+    except ZeroDivisionError:
+        print("Un dénominateur ne peut pas être égal à zéro.\n")
+        saisie_2eme_fractions()
+    except Exception as e:
+        print(type(e))
+        print(e)
+
+
+#Fonction Principal
+def main():
+    
+    r1 = saisie_1er_fraction()
+    print("Le premier nombre rationnel saisi est : ", r1)
+
+    r2 = saisie_2eme_fractions()
+    print("Le deuxième nombre rationnel saisi est : ", r2)
+
 
 main()
